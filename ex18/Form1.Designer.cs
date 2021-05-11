7fe78c8b3e9aa19e361f2ea87e286cf38bf7658d
JosiaAlmeida <>
2021-05-11 16:52:27 +0100

    Ex18

---
+++ ex18/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace ex18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(145, 13);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(183, 20);
+            this.txtEntrada.TabIndex = 0;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(75, 311);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(155, 23);
+            this.btnClick.TabIndex = 1;
+            this.btnClick.Text = "Converter por extenso";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numero";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(16, 108);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(312, 20);
+            this.txtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 346);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.txtEntrada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
