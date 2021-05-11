e3d42d8d61aec29c3c361ef2c127e3f0f9e863f4
JosiaAlmeida <>
2021-05-11 14:38:39 +0100

    Novo exercicio|Ex24 - Completo

---
+++ ex24/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace ex24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSaida = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(98, 5);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(210, 20);
+            this.txtEntrada.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Saida";
+            // 
+            // txtSaida
+            // 
+            this.txtSaida.Location = new System.Drawing.Point(98, 146);
+            this.txtSaida.Name = "txtSaida";
+            this.txtSaida.Size = new System.Drawing.Size(210, 20);
+            this.txtSaida.TabIndex = 3;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(122, 295);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 4;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 330);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.txtSaida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSaida;
+        private System.Windows.Forms.Button btnClick;
+    }
+}
+
---
+++ ex24/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            string[] texto = txtEntrada.Text.Split(' ');
+            Array.Reverse(texto);
+            string join = String.Join(",", texto).Replace(",", " ");
+            txtSaida.Text = join;
+        }
+    }
+}
