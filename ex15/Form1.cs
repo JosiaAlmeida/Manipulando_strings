4da8f6c17bc0f855e3564c0f84ab2a912eec26d4
JosiaAlmeida <>
2021-05-11 17:24:03 +0100

    Ex15 - Completo

---
+++ ex15/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            txtSaida.Text = ToUpperCase(txtEntrada.Text);
+        }
+        private string ToUpperCase(string txt)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(txt.ToLower());
+        }
+    }
+}
