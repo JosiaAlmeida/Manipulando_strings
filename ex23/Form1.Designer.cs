580d602a1d5f55a6c9e604333c4de5623dad26a8
JosiaAlmeida <>
2021-05-11 16:24:57 +0100

    Adicionando ex23

---
+++ ex23/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace ex23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtRestul = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto :";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(106, 10);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(249, 20);
+            this.txtEntrada.TabIndex = 1;
+            this.txtEntrada.BindingContextChanged += new System.EventHandler(this.TextChanged);
+            // 
+            // txtRestul
+            // 
+            this.txtRestul.Location = new System.Drawing.Point(106, 151);
+            this.txtRestul.Name = "txtRestul";
+            this.txtRestul.Size = new System.Drawing.Size(249, 20);
+            this.txtRestul.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 334);
+            this.Controls.Add(this.txtRestul);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.TextBox txtRestul;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ex23/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtEntrada.TextChanged += TextChanged;
+        }
+
+        private void TextChanged(object sender, EventArgs e)
+        {
+            txtRestul.Text = txtEntrada.Text.Replace(" ", "");
+        }
+    }
+}
